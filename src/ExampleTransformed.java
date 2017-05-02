2f0a6733fc0825eda5d42acda58f10f4ca9d8556
RakshaBC <b.raskc@gmail.com>
2017-05-01 19:20:13 -0500

    committ final
    final changes

---
+++ src/ExampleTransformed.java
@@ -0,0 +1,17 @@
+public class ExampleTransformed {
+	public static void main(String[] args) {
+		System.out.println("main() executed");
+		m1();
+		m2();
+	}
+
+	public static void m1() {
+		System.out.println("m1() executed");
+	}
+
+	public static void m2() {
+		System.out.println("m2() executed");
+		m1();
+		m1();
+	}
+}
